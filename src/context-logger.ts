5593ba0ca0c1f67d4df65fd1112583b8e0c6eb48
José Joaquín Bocanegra <bocanegra.velasco@gmail.com>
2025-04-25 14:34:04 +0100

    fix: `fatal` log pass error object  (#12)
    * feature: allows `fatal` to receive error
    
    * chore: refactor `context-logger` to avoid duplicated code
    
    * refactor: removes newly added method and use the same code in `fatal` and `error` to make it more readable

--- src/context-logger.ts
+++ src/context-logger.ts
@@ -54,8 +54,26 @@ export class ContextLogger {
     this.callInternalLogger('warn', message, (bindings ?? {}));
   }
 
-  fatal(message: string, bindings?: Bindings) {
-    this.callInternalLogger('fatal', message, (bindings ?? {}));
+  fatal(message: string): void;
+  fatal(message: string, error: Error): void;
+  fatal(message: string, bindings: Bindings): void;
+  fatal(message: string, error: Error, bindings: Bindings): void;
+  fatal(message: string, errorOrBindings?: Error | Bindings, bindings?: Bindings): void {
+    let error: Error | undefined;
+    const adaptedBindings: Bindings = {};
+
+    if (errorOrBindings instanceof Error) {
+      error = errorOrBindings;
+      if (bindings) {
+        Object.assign(adaptedBindings, bindings);
+      }
+    } else if (typeof errorOrBindings === 'string') {
+      error = errorOrBindings;
+    } else if (errorOrBindings) {
+      Object.assign(adaptedBindings, errorOrBindings);
+    }
+
+    this.callInternalLogger('fatal', message, adaptedBindings, error);
   }
 
   error(message: string): void;
@@ -63,19 +81,20 @@ export class ContextLogger {
   error(message: string, bindings: Bindings): void;
   error(message: string, error: Error, bindings: Bindings): void;
   error(message: string, errorOrBindings?: Error | Bindings, bindings?: Bindings): void {
-    let error;
-    const adaptedBindings = {};
+    let error: Error | undefined;
+    const adaptedBindings: Bindings = {};
+
     if (errorOrBindings instanceof Error) {
       error = errorOrBindings;
       if (bindings) {
         Object.assign(adaptedBindings, bindings);
       }
-      // Bootstrapping logs
     } else if (typeof errorOrBindings === 'string') {
       error = errorOrBindings;
     } else if (errorOrBindings) {
       Object.assign(adaptedBindings, errorOrBindings);
     }
+
     this.callInternalLogger('error', message, adaptedBindings, error);
   }
 
