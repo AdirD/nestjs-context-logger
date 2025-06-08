9f88639b5ba3997dd9ab5b71472288ef1b915574
Idan Ben Ami <idbenami27@gmail.com>
2025-06-08 14:44:54 +0300

    chore: update nest v11 example to use pino v9

--- examples/nestjs11-example/src/main.ts
+++ examples/nestjs11-example/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { ContextLogger } from 'nestjs-context-logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
   const logger = new ContextLogger('Bootstrap');
+  const app = await NestFactory.create(AppModule, {
+    logger: logger,
+    bufferLogs: true,
+  });
+  
   logger.log('Application started');
   
   await app.listen(3000);
